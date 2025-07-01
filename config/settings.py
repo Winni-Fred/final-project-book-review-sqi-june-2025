4d5c4d04276b65a98d5a3f5570f2a2b9b514f41b
Winni-Fred <wigboama@gmail.com>
2025-07-01 12:02:06 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,10 @@ DEBUG = True if os.environ.get('DEBUG').lower() == "true" else False
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
