21de2244bb1089f3662dff4c0edd690ed601bd50
Winni-Fred <wigboama@gmail.com>
2025-07-01 12:17:45 +0100

    Update build.sh
    add code for superuser creation

--- build.sh
+++ build.sh
@@ -6,3 +6,17 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+# Create default superuser
+python manage.py shell <<EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'adminpass')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+EOF
